7e64c86f336f48e801a24ca995160af08d222b6b
vedansh tiwari <vt2855028@gmail.com>
2024-12-21 14:26:13 +0530

    Initial commit After add css file

--- csvdata_project/csvdata_project/settings.py
+++ csvdata_project/csvdata_project/settings.py
@@ -125,6 +125,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Path to the media directory
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
